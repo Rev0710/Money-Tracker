34d772a62ec786b597b2861aa37e5db1a3e81fe3
Rev0710 <delarosa.johnrev071004@gmail.com>
2026-04-10 09:01:10 +0800

    Update API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://money-tracker-n1vp.onrender.com/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
