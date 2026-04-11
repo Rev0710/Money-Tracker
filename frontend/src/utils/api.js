a10db37c1decd2774b48aee6c2910044411fa775
Rev0710 <delarosa.johnrev071004@gmail.com>
2026-04-11 15:26:38 +0800

    Updated code and fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,39 +1,72 @@
 import axios from 'axios';
 
-const api = axios.create({ 
+const api = axios.create({
   baseURL: 'https://money-tracker-n1vp.onrender.com/api'
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return Promise.reject(error);
-  }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
+export const getSummary = async () => {
+  const response = await api.get('/summary');
+  return response.data;
+};
+export const getBudgets = async () => {
+  const response = await api.get('/budgets');
+  return response.data;
+};
 
-export const getSummary = (month, year) => api.get(`/summary?month=${month}&year=${year}`);
-export const getTransactions = (params) => api.get('/transactions', { params });
-export const createTransaction = (data) => api.post('/transactions', data);
-export const updateTransaction = (id, data) => api.put(`/transactions/${id}`, data);
-export const deleteTransaction = (id) => api.delete(`/transactions/${id}`);
+export const createBudget = async (data) => {
+  const response = await api.post('/budgets', data);
+  return response.data;
+};
 
-export const getBudgets = (month, year) => api.get(`/budgets?month=${month}&year=${year}`);
-export const createBudget = (data) => api.post('/budgets', data);
-export const deleteBudget = (id) => api.delete(`/budgets/${id}`);
+export const getTransactions = async () => {
+  const response = await api.get('/transactions');
+  return response.data;
+};
 
-export const getGoals = () => api.get('/goals');
-export const createGoal = (data) => api.post('/goals', data);
-export const updateGoal = (id, data) => api.put(`/goals/${id}`, data);
-export const deleteGoal = (id) => api.delete(`/goals/${id}`);
-
+export const addTransaction = async (data) => {
+  const response = await api.post('/transactions', data);
+  return response.data;
+};
+export const deleteTransaction = async (data) => {
+  const response = await api.post('/transactions', data);
+  return response.data;
+};
+export const updateTransaction = async (data) => {
+  const response = await api.post('/transactions', data);
+  return response.data;
+};
+export const createTransaction = async (data) => {
+  const response = await api.post('/transactions', data);
+  return response.data;
+};
+export const deleteBudget = async (data) => {
+  const response = await api.post('/budgets', data);
+  return response.data;
+};
+export const getGoals = async (data) => {
+  const response = await api.post('/goals', data);
+  return response.data;
+};
+export const updateGoal = async (data) => {
+  const response = await api.post('/goals', data);
+  return response.data;
+};
+export const createGoal = async (data) => {
+  const response = await api.post('/goals', data);
+  return response.data;
+};
+export const deleteGoal = async (data) => {
+  const response = await api.post('/goals', data);
+  return response.data;
+};
 export default api;
